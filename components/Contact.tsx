702c16d5797f10226f52ed1ee1378850610b468c
Mosaad Ahmed Mohammed <mosaadahmed@Mosaads-MacBook-Air.local>
2025-09-04 16:17:16 -0400

    added name and email in contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -32,7 +32,7 @@ export default function Contact() {
     <section className="contact" id="contact">
       <h2>Contact Me</h2>
       <form ref={form} onSubmit={sendEmail} className="contact-form">
-      <input type="text" name="user_name" placeholder="Your Name" required />
+      <input type="text" name="name" placeholder="Your Name" required />
         <input type="email" name="user_email" placeholder="Your Email" required />
         <textarea name="message" placeholder="Your Message" required />
         <button type="submit">Send</button>
